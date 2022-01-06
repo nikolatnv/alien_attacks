7d9ec7b05312e7e1bb392c35ec0ac1ca32900f27
nikolatnv <nikolatnv@gmail.com>
2022-01-06 19:20:40 +0300

    добавил главный класс игры

--- attack_alien/game.py
+++ attack_alien/game.py
@@ -1,1 +1,21 @@
-print('hello')
+import sys, pygame
+
+
+class AlienAttack:
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Вороги напали!")
+
+    def run_game(self):
+        while True:
+            for even in pygame.event.get():
+                if even.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    aa = AlienAttack()
+    aa.run_game()
