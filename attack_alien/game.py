e8af762714a2baa0432c7d369a41bb9868a343b2
nikolatnv <nikolatnv@gmail.com>
2022-01-06 22:59:08 +0300

    написан класс настройки, корабль, добавлены методы

--- attack_alien/game.py
+++ attack_alien/game.py
@@ -10,17 +10,46 @@ class AlienAttack:
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # FULLSCREEN
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Вороги напали!")
         self.ship = Ship(self)
 
     def run_game(self):
         while True:
-            for even in pygame.event.get():
-                if even.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            self.ship.biltme()
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.biltme()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
--- attack_alien/ship.py
+++ attack_alien/ship.py
@@ -4,11 +4,22 @@ import pygame
 class Ship():
     def __init__(self, aa_game):
         self.screen = aa_game.screen
+        self.settings = aa_game.settings
         self.screen_rect = aa_game.screen.get_rect()
         self.img_ship = pygame.image.load('images/ship100.bmp').convert()
         self.img_ship.set_colorkey((255, 255, 255))
         self.rect = self.img_ship.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def biltme(self):
         self.screen.blit(self.img_ship, self.rect)
